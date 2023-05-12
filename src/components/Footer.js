8d5f7bfd4921d4ca89bbd10d1c84e20fefd9fb77
Jishnu <jishnu.dev.roy@gmail.com>
2023-05-12 17:09:18 +0530

    hi acho

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { theme } from '../Theme/Theme';
 
 function Footer(props) {
   const myContext = useContext(UserContext)
-  const { darkMode, setDarkMode } = myContext
+  const { darkMode } = myContext
   const date = new Date()
   return (
     <footer className={`flex bg-gray-100 justify-center p-6 shadow select-none ${darkMode ? theme['dark'] : ''}`}>
